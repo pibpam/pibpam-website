f655cad4a4026cf9b1cae81be3c216b8ecffcc6b
Lucas Dias <lucasmg37@gmail.com>
2023-04-25 15:25:34 -0300

    add first version of loading

---
+++ components/Loading/index.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from "react";
+import styles from "../../styles/components/Loading.module.scss"
+import useLoading from "../../hooks/useLoading";
+
+const Loading: React.FC = () => {
+    const {isLoading} = useLoading()
+
+    const [open, setOpen] = useState(isLoading)
+    const [closing, setClosing] = useState(false)
+
+    useEffect(() => {
+        if (!isLoading) {
+            setClosing(true)
+            setTimeout(() => {
+                setOpen(false)
+                setClosing(false)
+            }, 200)
+        } else {
+            setOpen(true)
+            setClosing(false)
+        }
+    }, [isLoading])
+
+    if (!open) {
+        return null
+    }
+
+    return (
+        <div className={`${styles.container}  ${closing && styles.closed}`}>
+            Loading
+        </div>
+    )
+}
+
+export default Loading
