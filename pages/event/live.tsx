2fcec716cbf87b86687622dc4b3a568d68782ed1
Lucas Dias <lucasmg37@gmail.com>
2023-05-15 13:29:14 -0300

    fix header

--- pages/event/live.tsx
+++ pages/event/live.tsx
@@ -1,5 +1,4 @@
 import type {NextPage} from 'next'
-import styles from '../../styles/Events.module.scss'
 import Website from '../../layout/container/Website'
 import DividerMobile, {EDividerColors} from "../../components/DividerMobile";
 import Header from "../../components/Header";
@@ -8,16 +7,20 @@ import HeaderPage from "../../components/HeaderPage";
 import LivePage from "../../components/LivePage";
 import {useContext} from "react";
 import {LivesContext} from "../../contexts/lives";
+import useHeader from "../../hooks/useHeader";
+import HeaderContainer from "../../components/HeaderContainer";
 
 const Live: NextPage = () => {
     const {open, toggleMenu} = useMenu()
     const {lives} = useContext(LivesContext)
+    const {scrollActive, changeScroll} = useHeader()
+
     return (
-        <Website openMenu={open} toggleMenu={toggleMenu}>
+        <Website changeScroll={changeScroll} openMenu={open} toggleMenu={toggleMenu}>
             <>
-                <div className={styles.header_container}>
+                <HeaderContainer active={scrollActive}>
                     <Header toggleMenu={toggleMenu}/>
-                </div>
+                </HeaderContainer>
                 <HeaderPage/>
                 <DividerMobile color={EDividerColors.white}/>
                 {!!lives.length &&
