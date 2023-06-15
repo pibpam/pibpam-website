75022aa34cd1d4a4f4a041f7b95b2a86a7485d65
Lucas Dias <lucasmg37@gmail.com>
2023-06-14 22:55:58 -0300

    gta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type {AppProps} from 'next/app'
 import Contexts from "../contexts";
 import Loading from "../components/Loading";
 import React, {useEffect} from "react";
+import Script from "next/script";
 
 function MyApp({Component, pageProps}: AppProps) {
     useEffect(() => {
@@ -15,12 +16,27 @@ function MyApp({Component, pageProps}: AppProps) {
     }, []);
 
     return (
-        <Contexts>
-            <>
-                <Loading/>
-                <Component {...pageProps} />
-            </>
-        </Contexts>
+        <>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-7HTJQHF1X6"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+        
+                  gtag('config', 'G-7HTJQHF1X6');
+                `}
+            </Script>
+            <Contexts>
+                <>
+                    <Loading/>
+                    <Component {...pageProps} />
+                </>
+            </Contexts>
+        </>
     )
 }
 
