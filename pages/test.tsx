b079cabb920b2225866b01472d8fda17d33f800d
Lucas Dias <lucasmg37@gmail.com>
2023-07-20 21:12:12 -0300

    test

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,8 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import type { NextPage } from 'next'
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import styles from "../styles/Privacy.module.scss"
+import { AppContext } from '../contexts/app';
 
 const Privacy: NextPage = () => {
+  const { isApp } = useContext(AppContext)
+
+  useEffect(() => {
+    if (!isApp) {
+      window.open('pibpamapp://path/into/app?hello=world')
+    }
+  }, [])
+
   return (
     <div className={styles.container}>
       <a href="pibpamapp://path/into/app?hello=world">Teste</a>
