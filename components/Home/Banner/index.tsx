2565ec67df646ec44b05655b4e87b9b747334d99
Lucas Dias <lucasmg37@gmail.com>
2024-03-29 23:15:26 -0300

    new home

--- components/Home/Banner/index.tsx
+++ components/Home/Banner/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 
 import styles from '../../../styles/components/Home/Banner.module.scss'
+import YTPlayer from "../../YTPlayer";
 
 const Banner: React.FC = () => {
-    return (
-        <div className={styles.banner}>
-            <div className={styles.content} >
-                <h1>Bem-vindo à Primeira Igreja Batista em Pará de Minas.</h1>
-                <h2>Um lugar para você e sua família!</h2>
-            </div>
-            <div className={styles.backdrop}  />
+  return (
+    <div className={styles.banner}>
+      <div className={styles.videoContainer}>
+        <div>
+          <YTPlayer videoId="UEiSGcEvyk8" autoplay controls={0} loop mute />
         </div>
-    )
+      </div>
+      <div className={styles.content} >
+        <h1>Bem-vindo à Primeira Igreja Batista em Pará de Minas.</h1>
+        <h2>Um lugar para você e sua família!</h2>
+      </div>
+      <div className={styles.backdrop} />
+    </div>
+  )
 }
 
 export default Banner
--- components/YTPlayer.tsx
+++ components/YTPlayer.tsx
@@ -1,46 +1,59 @@
-import React, {useRef, useState} from 'react';
-import {FiPlayCircle} from 'react-icons/fi';
-import YouTube, {YouTubePlayer} from 'react-youtube';
+import React, { useRef, useState } from 'react';
+import { FiPlayCircle } from 'react-icons/fi';
+import YouTube, { YouTubePlayer } from 'react-youtube';
 import styles from '../styles/components/YTPlayer.module.scss'
 
 interface YTPlayerProps {
-    videoId: string;
-    autoplay?: boolean;
-    thumb?: string
+  videoId: string;
+  autoplay?: boolean;
+  thumb?: string
+  controls?: number
+  loop?: boolean
+  mute?: boolean
 }
 
-const YTPlayer: React.FC<YTPlayerProps> = ({videoId, autoplay = false, thumb}: YTPlayerProps) => {
-    const [, setPlayer] = useState<YouTubePlayer>();
+const YTPlayer: React.FC<YTPlayerProps> = ({ videoId, autoplay = false, thumb, controls = 2, loop = false, mute = false}: YTPlayerProps) => {
+  const [player, setPlayer] = useState<YouTubePlayer>();
 
-    const [playing, setPlaying] = useState(true)
-    const playerContainer = useRef<null | HTMLDivElement>(null)
+  const [playing, setPlaying] = useState(true)
+  const playerContainer = useRef<null | HTMLDivElement>(null)
 
-    const liberaPlayer = () => {
-        setPlaying(true)
+  const liberaPlayer = () => {
+    setPlaying(true)
+  }
+
+  const handleOnEnd = () => {
+    if (player && loop){
+      player?.playVideo()
     }
+  }
 
-    return (
-        <div ref={playerContainer} className={styles.container}>
-            {thumb && !playing ? (
-                <div className={styles.video__backdrop} style={{background: "url('" + thumb + "') center/cover"}}>
-                    <button onClick={liberaPlayer}>
-                        <FiPlayCircle/>
-                    </button>
-                </div>
-            ) : (
-                <YouTube
-                    videoId={videoId}
-                    opts={{
-                        playerVars: {
-                            autoplay,
-                            controls: 2,
-                        },
-                    }}
-                    className="container"
-                    onReady={(event) => setPlayer(event.target)}
-                />
-            )}
-        </div>);
+  return (
+    <div ref={playerContainer} className={styles.container}>
+      {thumb && !playing ? (
+        <div className={styles.video__backdrop} style={{ background: "url('" + thumb + "') center/cover" }}>
+          <button onClick={liberaPlayer}>
+            <FiPlayCircle />
+          </button>
+        </div>
+      ) : (
+        <YouTube
+          videoId={videoId}
+          opts={{
+            playerVars: {
+              autoplay: +autoplay,
+              controls,
+              loop: +loop,
+              playsinline: 1,
+              mute: +mute
+            },
+          }}
+          className="container"
+          onReady={(event) => setPlayer(event.target)}
+          onEnd={handleOnEnd}
+        />
+      )}
+    </div>);
 };
 
 export default YTPlayer;
