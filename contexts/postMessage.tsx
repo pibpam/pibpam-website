e486bee9e3282d0b943eb8acceb84e605721c566
Lucas Dias <lucasmg37@gmail.com>
2026-04-19 19:48:00 -0300

    feat: adicionar alerta para ação no contexto de mensagens

--- contexts/postMessage.tsx
+++ contexts/postMessage.tsx
@@ -79,6 +79,9 @@ export const PostMessageContextProvider: React.FC<IChildren> = ({
   const [deviceInfo, setDeviceInfo] = useState<DeviceInfo | null>(null);
 
   useEffect(() => {
+
+    alert(action);
+
     if (action === EActions.GOBACK) {
       goBack({}).then();
       setAction("");
